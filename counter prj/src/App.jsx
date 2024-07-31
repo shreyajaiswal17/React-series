a938a979a2c3a3d440452ea91f2c052efe52867b
Shreya Jaiswal <shreyaa.we@gmail.com>
2024-07-31 14:14:03 +0530

    Counter prj created

--- counter prj/src/App.jsx
+++ counter prj/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+          //  counter
 function App() {
   
   let [counter, SetCounter] = useState(15)
