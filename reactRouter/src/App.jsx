f16e8e8baa2815f40f53c88841eb18f73dbd2df3
Shreya Jaiswal <shreyaa.we@gmail.com>
2024-11-21 16:48:44 +0530

    created Router

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-700 p-4'>reactRouter</h1>
+     
     </>
   )
 }
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './components/About/About'
+import Home from './components/Home/Home'
+const router = createBrowserRouter([
+  {
+    // top level element ke anadr nesting ho rhi hai
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path:"",
+        element: <Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
 
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
