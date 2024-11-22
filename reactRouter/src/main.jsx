03cfca41f99469faf627f0cf2bc7d913fc20007a
Shreya Jaiswal <shreyaa.we@gmail.com>
2024-11-22 12:54:53 +0530

    made user component

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,28 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import About from './components/About/About'
 import Home from './components/Home/Home'
-const router = createBrowserRouter([
-  {
-    // top level element ke anadr nesting ho rhi hai
-    path: '/',
-    element: <Layout/>,
-    children: [
-      {
-        path:"",
-        element: <Home/>
-      },
-      {
-        path:"about",
-        element:<About/>
-      },
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
 
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     // top level element ke anadr nesting ho rhi hai
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path:"",
+//         element: <Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       },
+
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout/>}>
+      <Route path="" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+      <Route path="user/:userid" element={<User/>}/>
+    </Route>
+  )
+);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <RouterProvider router={router}/>
