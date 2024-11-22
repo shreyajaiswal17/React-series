2b690f4fe46541458c5aa990432b3828d24c0322
Shreya Jaiswal <shreyaa.we@gmail.com>
2024-11-22 19:05:52 +0530

    react router

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import About from './components/About/About'
 import Home from './components/Home/Home'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -35,9 +36,15 @@ const router = createBrowserRouter(
       <Route path="about" element={<About/>}/>
       <Route path="contact" element={<Contact/>}/>
       <Route path="user/:userid" element={<User/>}/>
+      <Route 
+      loader ={githubInfoLoader}
+      path="github" 
+      element={<Github/>}
+      />
     </Route>
   )
 );
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <RouterProvider router={router}/>
